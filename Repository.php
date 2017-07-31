02ba7165e7df2b7981936f3338615473f4dcf9f4
Ivan Vegner <vegner@slrealty.ru>
2017-07-31 16:00:47 +0300

    Move formatting functions to a separate class

--- Repository.php
+++ Repository.php
@@ -2,6 +2,7 @@
 
 namespace SD\Currency;
 
+use SD\Currency\Service\Formatter;
 use SD\DependencyInjection\DeclarerInterface;
 use SD\DependencyInjection\ContainerAwareTrait;
 
@@ -61,46 +62,8 @@ class Repository implements DeclarerInterface {
         // return $this->container->produce(SD_Currency_Service_Store_Db::class);
     }
 
-    public function formatPrice(string $price, string $symbol): string {
-        if ($symbol === '&#8381;') { // rub
-            $format = 'PRICE_SYMBOL';
-        } elseif (in_array($symbol, array(
-            '&#36;',    // usd
-            '&#8364;',  // euro
-            '&euro;',
-        ))) {
-            $format = 'SYMBOL_PRICE';
-        } else {
-            // Цена по запросу
-            $format = 'PRICE';
-        }
-        if ($format === 'PRICE') {
-            $output = esc_attr($price);
-        } else {
-            $separator = '&thinsp;';
-            $formatted = preg_replace(
-                '/(?<=\d)&(?=\d)/',
-                $separator,
-                number_format((int)$price, 0, '.', $separator)
-            );
-            $fontAwesome = $this->getFontAwesome($symbol);
-            $parts = [];
-            if ($format === 'PRICE_SYMBOL') {
-                $parts = [$formatted, $fontAwesome];
-            } elseif ($format === 'SYMBOL_PRICE') {
-                $parts = [$fontAwesome, $formatted];
-            }
-            $output = implode($parts, '&nbsp;');
-        }
-        return $output;
-    }
-
-    public function getFontAwesome($symbol) {
-        $config = $this->getConfigBySymbol($symbol);
-        if ($config) {
-            return '<i class="fa fa-' . strtolower($config->getCode()) . '" aria-hidden="true"></i>';
-        }
-        return '';
+    public function getFormatter() {
+        return new Formatter();
     }
 
     public function getConfigByCode(string $code) {
---
+++ Service/Formatter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace SD\Currency\Service;
+
+class Formatter {
+    public function formatPrice(string $price, string $symbol): string {
+        if ($symbol === '&#8381;') { // rub
+            $format = 'PRICE_SYMBOL';
+        } elseif (in_array($symbol, array(
+            '&#36;',    // usd
+            '&#8364;',  // euro
+            '&euro;',
+        ))) {
+            $format = 'SYMBOL_PRICE';
+        } else {
+            // Цена по запросу
+            $format = 'PRICE';
+        }
+        if ($format === 'PRICE') {
+            $output = esc_attr($price);
+        } else {
+            $separator = '&thinsp;';
+            $formatted = preg_replace(
+                '/(?<=\d)&(?=\d)/',
+                $separator,
+                number_format((int)$price, 0, '.', $separator)
+            );
+            $fontAwesome = $this->getFontAwesome($symbol);
+            $parts = [];
+            if ($format === 'PRICE_SYMBOL') {
+                $parts = [$formatted, $fontAwesome];
+            } elseif ($format === 'SYMBOL_PRICE') {
+                $parts = [$fontAwesome, $formatted];
+            }
+            $output = implode($parts, '&nbsp;');
+        }
+        return $output;
+    }
+
+    public function getFontAwesome($symbol) {
+        $config = SD_Currency_Model_Config::getBySymbol($symbol);
+        if ($config) {
+            return '<i class="fa fa-' . strtolower($config->getCode()) . '" aria-hidden="true"></i>';
+        }
+        return '';
+    }
+}
