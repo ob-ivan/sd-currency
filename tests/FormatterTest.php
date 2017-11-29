bc5500332b28d0f42d282615c34b25854183ada1
Ivan Vegner <vegner@slrealty.ru>
2017-11-29 13:22:42 +0300

    Create test for format money

--- tests/FormatterTest.php
+++ tests/FormatterTest.php
@@ -2,6 +2,7 @@
 namespace tests;
 
 use PHPUnit\Framework\TestCase;
+use SD\Currency\Model\Money;
 use SD\Currency\Model\Registry;
 use SD\Currency\Service\Formatter;
 
@@ -57,6 +58,58 @@ class FormatterTest extends TestCase {
                 'symbol' => '&#8364;',
                 'expected' => '&#8364;&nbsp;271&thinsp;828',
                 'message' => 'Must disable font awesome if requested by config',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider formatMoneyDataProvider
+    **/
+    public function testFormatMoney($config, $money, $expected, $message) {
+        $registry = new Registry();
+        $formatter = new Formatter($registry, $config);
+        $this->assertEquals($expected, $formatter->formatMoney($money), $message);
+    }
+
+    public function formatMoneyDataProvider() {
+        $registry = new Registry();
+        return [
+            [
+                'config' => [
+                    'separator' => '+',
+                    'symbolType' => 'html',
+                ],
+                'money' => new Money(7777777, $registry->getByCode('RUB')),
+                'expected' => '7+777+777&nbsp;&#8381;',
+                'message' => 'Must place RUB html symbol after the number',
+            ],
+            [
+                'config' => [
+                    'separator' => '/',
+                    'symbolType' => 'unicode',
+                ],
+                'money' => new Money(123456, $registry->getByCode('USD')),
+                'expected' => '$&nbsp;123/456',
+                'message' => 'Must place USD unicode symbol before the number',
+            ],
+            [
+                'config' => [
+                    'symbolType' => 'fontAwesome',
+                ],
+                'money' => new Money(31415926, $registry->getByCode('EUR')),
+                'expected' => '<i class="fa fa-eur" aria-hidden="true"></i>&nbsp;31&thinsp;415&thinsp;926',
+                'message' => 'Must insert font awesome tag',
+            ],
+            [
+                'config' => [
+                    'symbolType' => 'map',
+                    'symbolMap' => [
+                        'RUB' => 'руб.',
+                    ],
+                ],
+                'money' => new Money(271828, $registry->getByCode('RUB')),
+                'expected' => '271&thinsp;828&nbsp;руб.',
+                'message' => 'Must get currency symbol from a map',
             ],
         ];
     }
