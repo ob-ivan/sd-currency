4b56daf6a56d8f20e70c4a6b580d920d7ce83ea2
Ivan Vegner <vegner@slrealty.ru>
2018-02-13 18:23:11 +0300

    Stub new provider implementation

--- src/DependencyInjection/CurrencyProvider.php
+++ src/DependencyInjection/CurrencyProvider.php
@@ -2,14 +2,28 @@
 namespace SD\Currency\DependencyInjection;
 
 use SD\Currency\Repository;
+use SD\DependencyInjection\AutoDeclarerInterface;
+use SD\DependencyInjection\AutoDeclarerTrait;
+use SD\DependencyInjection\ContainerAwareTrait;
 use SD\DependencyInjection\ProviderInterface;
 
-class CurrencyProvider implements ProviderInterface {
-    public function getServiceName(): string {
+class CurrencyProvider implements AutoDeclarerInterface, ProviderInterface
+{
+    use AutoDeclarerTrait;
+    use ContainerAwareTrait;
+
+    public function getServiceName(): string
+    {
         return 'currency';
     }
 
-    public function provide() {
-        return new Repository();
+    public function provide()
+    {
+        $config = $this->getConfig('currency');
+        $repository = new Repository();
+        if (isset($config['store']['class'])) {
+            $repository->setStore($this->getContainer()->produce($config['store']['class']));
+        }
+        return $repository;
     }
 }
